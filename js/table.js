e65f6367eafa2534298ca02961974b6a22775234
pixscar <pie.dx96@gmail.com>
2025-05-14 17:58:32 +0200

    Aggiunge esercizio aggiunta tabella tramite JS

--- js/table.js
+++ js/table.js
@@ -18,3 +18,23 @@ const products = [
         categoryName: "Sport"
     }
 ];
+
+const b = document.querySelector("button");
+b.addEventListener("click", () => {
+	const tbody = document.querySelector("tbody");
+	products.forEach((product) => {
+	/* console.log(product); */
+		const tr = document.createElement("tr");
+		Object.keys(product).forEach((key) => {
+			const td = document.createElement("td");
+			if(key == "productId") {
+				td.classList.add("hide");
+				/* td.style = "display: none"; */ //implementato in CSS
+			}
+			td.textContent = product[key];
+			tr.appendChild(td);
+		});
+		tbody.appendChild(tr);
+	});
+});
+
