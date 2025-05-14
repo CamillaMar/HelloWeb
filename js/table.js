95d1f626c4e1513ee49bc442e9012adeb1e2fc32
LorenzoCoretti1 <lorenzo.coretti1@gmail.com>
2025-05-14 18:09:06 +0200

    esercizio table

--- js/table.js
+++ js/table.js
@@ -18,3 +18,28 @@ const products = [
         categoryName: "Sport"
     }
 ];
+const button = document.querySelector("button");
+button.addEventListener("click", ()=>{
+    const table = document.querySelector("table");
+    const newThead = document.createElement("thead");
+    const keys = Object.keys(products[0]);
+    keys.forEach(attribute => {
+        const newTh = document.createElement("th");
+        newTh.innerText = attribute;
+        newThead.appendChild(newTh);
+    });
+    table.appendChild(newThead);
+    const newTBody = document.createElement("tbody");
+    products.forEach(product => {
+        const newTr = document.createElement("tr");
+        newTr.classList.add("borderTr");
+        const values = Object.values(product);
+        values.forEach(data => {
+            const newTd = document.createElement("td");
+            newTd.innerText = data;
+            newTr.appendChild(newTd);
+        });
+        newTBody.appendChild(newTr);
+    });
+    table.appendChild(newTBody);
+});
