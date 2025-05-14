ec5e0b61744ab5763f994ca7711025ec3b50cf63
Kraiimen <78323935+Kraiimen@users.noreply.github.com>
2025-05-14 16:54:12 +0200

    Aggiunge il css alla lista

--- js/TODO.js
+++ js/TODO.js
@@ -4,15 +4,18 @@ const ul = document.querySelector('ul');
 
 btn.addEventListener('click', () => {
     const newListItem = document.createElement('li');
+    const newParagraph = document.createElement('p');
     const newButton = document.createElement('button');
 
-    newListItem.textContent = input.value + ' ';
+    newParagraph.textContent = input.value + ' ';
+    input.value = '';
 
     newButton.textContent = 'Cancella';
     newButton.addEventListener('click', () => {
         newListItem.remove();
     });
 
+    newListItem.appendChild(newParagraph);
     newListItem.appendChild(newButton);
     ul.appendChild(newListItem);
 });
