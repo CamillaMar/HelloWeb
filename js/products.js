5a3ca7efc887b40a3b077bbd88493735dc7a2f32
EmanueleGiustiniani <giustinianiemanuele0@gmail.com>
2025-05-16 00:14:29 +0200

    Aggiunge esercizio 2 di products

--- js/products.js
+++ js/products.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const b1 = document.querySelector("#esercizio-1");
-	const b2 = document.querySelector("#esercizio-2")
+	const b2 = document.querySelector("#esercizio-2");
 	const b3 = document.querySelector("#esercizio-3");
 
 	b1.addEventListener("click", () => {
-		loadData("topN", 10);
+		loadData("topN", 10, "GET");
 	});
 
 	b2.addEventListener("click", () => {
 		const input = document.querySelector("input");
-		loadData("categoryId", input.value);
+		loadData("categoryId", input.value, "POST");
 	});
 
 	b3.addEventListener("click", () => {
@@ -17,12 +17,24 @@ document.addEventListener("DOMContentLoaded", () => {
 	});
 });
 
-async function loadData(parameter, value){
+async function loadData(parameter, value, requestType){
     try {
-		let url = "http://localhost:8080/api/product";
-		const queryString = `?${parameter}=${value}`;
-		url += queryString; //VERIFICA += !
-        const response = await fetch(url);
+        let url = "http://localhost:8080/api/product";
+        if(requestType === "GET"){
+            const queryString = `?${parameter}=${value}`;
+		    url += queryString;
+        }
+        if(requestType === "POST"){
+            url += "/category";
+        }
+        const response = await fetch(url, {
+            method: requestType,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({parameter:value})//getJSONBody(requestType, value)
+        });
         if(!response.ok){
             throw new Error("http error "+ response.status);
         }
@@ -31,6 +43,13 @@ async function loadData(parameter, value){
     } catch(error){
         alert("errore di comunicazione con il server " + error);
     }
+}
+
+function getJSONBody(requestType, value){
+    if(requestType === "GET"){
+        return null;
+    }
+    return JSON.stringify(value);
 }
 
 function renderTable(data, containerId){
@@ -49,8 +68,9 @@ function renderTable(data, containerId){
     const headerRow = tHead.insertRow();
     const headers = Object.keys(data[0]);
     headers.forEach(header => {
-        const th = headerRow.insertCell();
+        const th = document.createElement("th");
         th.textContent = header.charAt(0).toUpperCase() + header.slice(1);
+        headerRow.appendChild(th);
     });
     const tBody = table.createTBody();
     data.forEach(item =>{
