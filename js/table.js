4b0a1a5a2231166ebda44ef0d7b1d4119dd2cdbb
pixscar <pie.dx96@gmail.com>
2025-05-15 09:41:07 +0200

    aggiunge js esercizio table

--- js/table.js
+++ js/table.js
@@ -1,20 +1,49 @@
 const products = [
-	{
-		productId: 1,
-		productName: "Bicicletta",
-		itemPrice: 200.00,
-		categoryname: "sport"
-	},
-	{
-		productId: 2,
-		productName: "Moto",
-		itemPrice: 8000.00,
-		categoryname: "sport"
-	},
-	{
-		productId: 3,
-		productName: "Pallone",
-		itemPrice: 15.00,
-		categoryname: "sport"
-	}	
+    {
+        productId: 1,
+        productName: "Bicicletta",
+        itemPrice: 200.00,
+        categoryName: "Sport"
+    },
+    {
+        productId: 2,
+        productName: "Scii",
+        itemPrice: 150.00,
+        categoryName: "Sport"
+    },
+    {
+        productId: 3,
+        productName: "Tavola da Surf",
+        itemPrice: 300.00,
+        categoryName: "Sport"
+    }
 ];
+
+const b = document.querySelector("button");
+let isAdded = false;
+
+b.addEventListener("click", () => {
+    if(isAdded){
+        return;
+    }
+    isAdded = true;
+
+	const tbody = document.querySelector("tbody");
+	products.forEach((product) => {
+	    /* console.log(product); */
+		const tr = document.createElement("tr");
+        
+		Object.keys(product).forEach((key) => {
+			const td = document.createElement("td");
+            
+			if(key == "productId") {
+				td.classList.add("hide");
+				/* td.style = "display: none"; */ //implementato in CSS
+			}
+			td.textContent = product[key];
+			tr.appendChild(td);
+		});
+		tbody.appendChild(tr);
+	});
+});
+
