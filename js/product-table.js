4a62e49b5c45e1c14c3b55ca079dd5da3433ea16
Colum-nar <edoardobognanni@gmail.com>
2025-05-16 15:11:30 +0100

    finito esercizio 2

--- js/product-table.js
+++ js/product-table.js
@@ -6,7 +6,7 @@ function renderTable(data, tableid) {
         console.log("L'array di dati è vuoto");
         return;
     } 
-    const table = document.querySelector(`${tableid}`);
+    const table = document.querySelector(`#${tableid}`);
     table.innerHTML = "";
     const tHead = table.createTHead();
     const tr = tHead.insertRow();
@@ -44,32 +44,61 @@ button.addEventListener("click", ()=>{
 
 //esercizio 2
 
-function loadAllCategories(data) {
+
+function renderCategorySelect(data) {
     if(data.length === 0) {
         console.log("L'array è vuoto");
         return
     }
     const select = document.querySelector("select");
-    const keys = Object.keys(data[0]);
     data.forEach(category => {
         const opt = document.createElement("option");
-        console.log(category.categoryId);
+        //console.log(category.categoryId);
+        opt.value=category.categoryId;
+        opt.textContent=category.categoryName;
+        select.appendChild(opt);
     });
-}
 
-async function loadProductByCategoryName(categoryName) {
+}
+async function loadCategories(){
     try {
-        const response = await fetch("http://localhost:8080/api/product?categoryName=" + categoryName);
+        const response = await fetch("http://localhost:8080/api/category");
+        if(!response.ok) {
+            throw new Error("Http Error");
+        } 
+        const data = await response.json();
+        renderCategorySelect(data);
+    } catch (error) {
+        alert("Errore Server" + error);
+    }
+}
+loadCategories();
+
+async function loadProductByCategoryId(categoryId) {
+    try {
+
+        const response = await fetch("http://localhost:8080/api/product?categoryId=" + categoryId);
+        const data = await response.json();
+        let somma = 0;
+        data.forEach(product =>{
+            somma += product.unitPrice;
+        });
+        if(data.length > 5 || somma > 100){
+            renderTable(data,"table2");
+        } else{
+            await postData(categoryId);
+
+            const response2 = await fetch("http://localhost:8080/api/product?categoryId=" + categoryId);
+            const data2 = await response2.json();
+            renderTable(data2,"table2");
+        }
     } catch (error) {
         alert("Errore Server" + error);
     } 
 }
 
-const input = document.querySelector("input");
-const button2 = document.querySelector("article>button");
-button2.addEventListener() //lasciamo qui un attimo 
 
-async function postData() {
+async function postData(categoryId) {
     try {
         const config = {
             method: 'POST',
@@ -77,23 +106,31 @@ async function postData() {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
-            body: {
+            body: JSON.stringify({
                 "productId": 0,
                 "productName": "standard product",
                 "supplierId": 1,
                 "unitPrice": 100,
-                "discontinued": false 
-            }
-        }
-        const response = await fetch(url, config)
+                "discontinued": false,
+                "categoryId": categoryId
+            })
+        };
+        const response = await fetch("http://localhost:8080/api/product" , config);
         //const json = await response.json()
         if (response.ok) {
             //return json
-            return response
+            return response;
         } else {
-            //
+            console.log("Http Error");
         }
     } catch (error) {
-            //
+            console.log("Erroe nel collegamento con il server: " + error);
     }
 }
+const select = document.querySelector("select");
+const button2 = document.querySelector("article>button");
+button2.addEventListener("click", function(){
+    const idValue = select.value;
+    loadProductByCategoryId(idValue);
+    
+}) //lasciamo qui un attimo 
