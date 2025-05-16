7ea806a1ca320f392967d49f0efdf04fa139f019
Colum-nar <edoardobognanni@gmail.com>
2025-05-16 12:26:53 +0100

    pippo

---
+++ js/prova.js
@@ -0,0 +1,11 @@
+const obj = {
+    name: 'pippo',
+    age: 20
+};
+
+console.log(obj.constructor == Object);
+console.log(obj.__proto__==Object.prototype);
+
+function Dev (firstnname, lastName, favouriteLanguage){
+    this.firstame
+}
