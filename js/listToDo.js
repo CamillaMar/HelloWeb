d64f43dc094fd2ad749eef5de650d60ef328c157
EmanueleGiustiniani <giustinianiemanuele0@gmail.com>
2025-05-22 13:00:01 +0200

    continua l'esercizio dei todo

---
+++ js/listToDo.js
@@ -0,0 +1,25 @@
+const b = document.querySelector("#send");
+b.addEventListener("click", () =>{
+    createTodo();
+    localStorage.setItem("todoList",JSON.stringify(todoList));
+});
+
+const todoList = [];
+
+function createTodo(){
+    const newTodo = {
+        title: document.querySelector("#title").value,
+        description: document.querySelector("#description").value,
+        creationDate: new Date(),
+        expireDate: document.querySelector("#expire-date").value,
+        completionDate: null,
+        category: document.querySelector("#category").value,
+        done: document.querySelector("#done").checked
+    };
+
+    if (newTodo.done) {
+        newTodo.completionDate = newTodo.creationDate;
+    }
+
+    todoList.push(newTodo);
+}
